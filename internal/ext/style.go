cff6786caee600bafa00a1673bac3705cbaa523d
Joseph Kato <joseph@jdkato.io>
2021-10-23 23:01:35 -0700

    More syntax changes

--- internal/ext/style.go
+++ internal/ext/style.go
@@ -11,5 +11,7 @@ var boldKeywords = styles.Register(chroma.MustNewStyle("bold-keywords", chroma.S
 	chroma.KeywordNamespace:   "bold",
 	chroma.KeywordReserved:    "bold",
 	chroma.Keyword:            "bold",
+	chroma.Operator:           "bold",
 	chroma.OperatorWord:       "bold",
+	chroma.NameBuiltin:        "bold",
 }))
